47e2a143014becb77fa3acafb00768dd526f9300
ehenry31 <66963551+ehenry31@users.noreply.github.com>
2020-06-17 14:48:03 +0200

    Update VigiFlore1.R

--- VigiFlore1.R
+++ VigiFlore1.R
@@ -47,41 +47,6 @@ sapply(traits570,function(x) sum(is.na(x))) #Nombreuses valeurs manquantes
 # sum(is.na(tendtempo$verification_esp)) # = 0, on retrouve bien les mêmes espèces 
 # tendtempo$verification_esp <- NULL
 
-# ENLEVER LES OUTLIERS
-
-# outlierKD <- function(dt, var) {
-#   var_name <- eval(substitute(var),eval(dt))
-#   na1 <- sum(is.na(var_name))
-#   m1 <- mean(var_name, na.rm = T)
-#   par(mfrow=c(2, 2), oma=c(0,0,3,0))
-#   boxplot(var_name, main="With outliers")
-#   hist(var_name, main="With outliers", xlab=NA, ylab=NA)
-#   outlier <- boxplot.stats(var_name)$out
-#   mo <- mean(outlier)
-#   var_name <- ifelse(var_name %in% outlier, NA, var_name)
-#   boxplot(var_name, main="Without outliers")
-#   hist(var_name, main="Without outliers", xlab=NA, ylab=NA)
-#   title("Outlier Check", outer=TRUE)
-#   na2 <- sum(is.na(var_name))
-#   cat("Outliers identified:", na2 - na1, "n")
-#   cat("Propotion (%) of outliers:", round((na2 - na1) / sum(!is.na(var_name))*100, 1), "n")
-#   cat("Mean of the outliers:", round(mo, 2), "n")
-#   m2 <- mean(var_name, na.rm = T)
-#   cat("Mean without removing outliers:", round(m1, 2), "n")
-#   cat("Mean if we remove outliers:", round(m2, 2), "n")
-#   response <- readline(prompt="Do you want to remove outliers and to replace with NA? [yes/no]: ")
-#   if(response == "y" | response == "yes"){
-#     dt[as.character(substitute(var))] <- invisible(var_name)
-#     assign(as.character(as.list(match.call())$dt), dt, envir = .GlobalEnv)
-#     cat("Outliers successfully removed", "n")
-#     return(invisible(dt))
-#   } else{
-#     cat("Nothing changed", "n")
-#     return(invisible(var_name))
-#   }
-# }
-# 
-# outlierKD(tendtempo, mean)
 
 ### Compléter les valeurs manquantes des traits
 
@@ -181,3 +146,4 @@ ggplot(test_tend_poll,aes(x=as.factor(info_entomogame), y=mean)) +
 #on observe pas de tendance particulière
 summary(lm(info_entomogame~mean,data=test_tend_poll))
 # p-value: 0.05965, effet non significatif du % d'info entomogame sur la tendance
+AIC(lm(info_entomogame~mean,data=test_tend_poll)) #AIC= 5544
